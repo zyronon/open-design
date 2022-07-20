07175081b217a54c6508d4a401a0334e70dbb3b9
zyronon <zyronon@163.com>
2022-07-20 11:58:36 +0800

    delete unnecessary files

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,12 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App'
-// import './index.css'
-//
-// ReactDOM.createRoot(document.getElementById('root')!).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// )
-
-
 import * as React from "react";
-import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./index.css";
 import 'moment/locale/zh-cn';
 import 'antd/dist/antd.css';
-import {createRoot} from "react-dom/client";
-import {store} from './store/store.js';
-import {Provider} from "react-redux";
+import { createRoot } from "react-dom/client";
+import { store } from './store/store.js';
+import { Provider } from "react-redux";
 import Canvas from "./pages/canvas/Canvas";
-// import Canvas from "./pages/canvas/Canvas-hooks";
-// import App from "./App";
 import Test from "./pages/test/Test";
 
 
--- src/router/asyncRoutes.ts
+++ src/router/asyncRoutes.ts
@@ -1,41 +1,5 @@
-import Home from "../pages/home/Home";
 import About from "../pages/about/About";
-import BaseLayout from "../pages/layout/BaseLayout";
-import Hooks from "../pages/hooks/Hooks";
-import Redux from "../pages/reduxPage/Redux";
-import Hoc from "../pages/hoc/Hoc";
 import Canvas from "../pages/canvas/Canvas";
 import Test from "../pages/test/Test-list-call"
 
-export default [
-  {
-    path: '/layout',
-    component: BaseLayout,
-    children: [
-      {
-        path: '/layout/home',
-        component: Home,
-      },
-      {
-        path: '/layout/about',
-        component: About,
-      },
-      {
-        path: '/layout/hooks',
-        component: Hooks,
-      },
-      {
-        path: '/layout/reduxPage',
-        component: Redux,
-      },
-      {
-        path: '/layout/hoc',
-        component: Hoc,
-      },
-      {
-        path: '/layout/canvas',
-        component: Canvas,
-      },
-    ]
-  },
-]
+export default []

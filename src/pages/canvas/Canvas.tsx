cd100f26edd40b3b4cefa08c830f0d19ae21f567
zyronon <zyronon@163.com>
2022-08-03 02:04:08 +0800

    添加左侧边栏

--- src/pages/canvas/Canvas.tsx
+++ src/pages/canvas/Canvas.tsx
@@ -1132,7 +1132,7 @@ class Canvas extends React.Component<any, IState> {
     // console.log('se', selectRect)
     const type = selectRect?.type
     return <>
-      <div className={'design dark'}>
+      <div className={'design'}>
         <div className="header">
           <div className={'fps'}>
             FPS:<Fps/>
--- src/pages/canvas/components/Left/left.tsx
+++ src/pages/canvas/components/Left/left.tsx
@@ -1,12 +1,100 @@
-import React, { memo } from "react"
-import { store } from "../../store"
+import React, {memo, useState} from "react"
+import {store} from "../../store"
 import './index.scss'
+import Icon from "@icon-park/react/es/all";
+
+const RectComponent = (props: any) => {
+  const [expand, setExpand] = useState<boolean>(true)
+
+  return (
+    <div className="component" {...props}>
+      <div className="top" style={{paddingLeft: (props.index + 1) * 20 + 'rem'}}>
+        {
+          props.item?.children &&
+            <Icon type={'right-one'} size={14} theme={'filled'} onClick={() => setExpand(!expand)}/>
+        }
+        <span className={'name'}>{props.item.name}</span>
+      </div>
+      {
+        expand && <div className="children">
+          {
+            props.item?.children?.map((item: any, index: any) => {
+              return <RectComponent
+                item={item}
+                key={index}
+                index={props.index + 1}
+              />
+            })
+          }
+          </div>
+      }
+    </div>
+  )
+}
 
 export default memo((props: any) => {
+  const components = [
+    {
+      name: '1',
+      children: [
+        {
+          name: '2'
+        },
+        {
+          name: '2'
+        },
+        {
+          name: '2'
+        },
+      ]
+    },
+    {
+      name: '1',
+      children: [
+        {
+          name: '2'
+        },
+        {
+          name: '2'
+        },
+        {
+          name: '2'
+        },
+      ]
+    },
+    {
+      name: '1',
+      children: [
+        {
+          name: '2'
+        },
+        {
+          name: '2'
+        },
+        {
+          name: '2'
+        },
+      ]
+    }
+  ]
   return (
-    <div className="left">
+    <div className="left-wrapper">
+      <div className="temp">
+        <div className="component" onClick={() => props.init()}>
+          刷新
+        </div>
+        <div className="component" onClick={() => props.navigate('/test')}>
+          去test
+        </div>
+        <div className="component" onClick={props.print}>
+          打印
+        </div>
+        <div className="component" onClick={() => console.log(store.rectList)}>
+          打印2
+        </div>
+      </div>
       <div className="tabs">
-        <div className="tab">
+        <div className={'tab active'}>
           图层
         </div>
         <div className="tab">
@@ -17,21 +105,40 @@ export default memo((props: any) => {
         </div>
       </div>
       <div className={'layer'}>
-        <div className="pages">
-
+        <div className="page-wrapper">
+          <div className="header">
+            <div className="left">
+              <div className="name">页面</div>
+            </div>
+            <div className="right">
+              <Icon type={'plus'} size="20"/>
+              <Icon type={'down'} size="20"/>
+            </div>
+          </div>
+          <div className="pages">
+            {
+              Array.from({length: 2}).map((v, i) => {
+                return <div className="page" key={i}>
+                  <Icon type={'check-small'} size="16"/>
+                  <span>Cover</span>
+                </div>
+              })
+            }
+          </div>
         </div>
-        <div className='components'>
-          <div className="component" onClick={() => props.init()}>
-            刷新
-          </div>
-          <div className="component" onClick={() => props.navigate('/test')}>
-            去test
-          </div>
-          <div className="component" onClick={props.print}>
-            打印
-          </div>
-          <div className="component" onClick={() => console.log(store.rectList)}>
-            打印2
+        <div className="search">
+          <Icon type={'search'} size="14"/>
+          <Icon type={'down'} size="14"/>
+          <input type="text" placeholder={'请输入'}/>
+          <Icon type={'close-one'} className={'close'} theme="filled" size="18" fill="#8f8f8f"/>
+        </div>
+        <div className="components">
+          <div className="scroll">
+            {
+              components.map((item, index) => {
+                return <RectComponent item={item} key={index} index={-1}/>
+              })
+            }
           </div>
         </div>
       </div>

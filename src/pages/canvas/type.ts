29d034d28f17c4abe43609c27317936ad6f0a724
zyronon <zyronon@163.com>
2022-11-29 18:22:35 +0800

    save

--- src/pages/canvas/type.ts
+++ src/pages/canvas/type.ts
@@ -124,11 +124,11 @@ export interface ShapeProps {
   parent?: ShapeConfig
 }
 
+//属性参考：https://developers.mastergo.com/apis/node-frame.html
 export interface ShapeConfig {
   id: number | string,
   name?: number | string,
-  x: number,
-  y: number,
+
   w: number,
   h: number,
   rx: number,
@@ -157,7 +157,90 @@ export interface ShapeConfig {
   bottomLeft: P,
   bottomRight: P,
   center: P,
+  isVisible: boolean,//节点是否可见
+  isLocked: boolean,//节点是否被锁定
+  /**
+   * @desc Geometry-related properties
+   * */
+  fills: any[]//图层的填充。
+  strokes: any[]//图层的描边。
+  /** @desc 描边类型。
+   * 'SOLID': 实线。
+   'DASH': 虚线。
+   'CUSTOM': 自定义。
+   * */
+  strokeStyle: 'SOLID' | 'DASH' | 'CUSTOM'
+  strokeWeight: number,//四个方向描边的粗细
+  strokeTopWeight: number,
+  strokeLeftWeight: number,
+  strokeRightWeight: number,
+  strokeBottomWeight: number,
+  /** @desc 描边相对于图层边界的对齐方式。
+   * 'CENTER': 居中。
+   * 'INSIDE': 内部。
+   * 'OUTSIDE': 外部。
+   * */
+  strokeAlign: 'CENTER' | 'INSIDE' | 'OUTSIDE'
+  /** @desc 端点的装饰。
+   * 'NONE': 正常。
+   * 'ROUND': 圆角。
+   * 'SQUARE': 方型。
+   * 'LINE_ARROW': 普通箭头。
+   * 'TRIANGLE_ARROW': 三角箭头。
+   * 'ROUND_ARROW' 圆箭头。
+   * 'RING' 圆环。
+   * 'DIAMOND' 方块。
+   * 'LINE' 直线。
+   * */
+  strokeCap: 'NONE' | 'ROUND' | 'SQUARE' | 'LINE_ARROW' | 'TRIANGLE_ARROW' | 'ROUND_ARROW' | 'RING' | 'DIAMOND' | 'LINE'
+  /** @desc 边角的装饰。
+   * 'MITER': 直角。
+   * 'BEVEL': 斜切。
+   * 'ROUND': 圆角。
+   * */
+  strokeJoin: 'MITER' | 'BEVEL' | 'ROUND'
+  strokeDashes: [number, number]//包含数字的数组。数组偶数下标元素代表虚线的长度，奇数下标元素代表虚线的间距。
+  dashCap: 'NONE' | 'ROUND' | 'SQUARE' //虚线端点装饰。
+  /**
+   *  @desc Corner-related properties
+   * */
+  cornerSmooth: number,//控制角的平滑程度，值域为 [0, 1]。
+  cornerRadius: number,//圆角。
+  topLeftRadius: number,
+  topRightRadius: number,
+  bottomLeftRadius: number,
+  bottomRightRadius: number,
+  /**
+   * @desc Blend-related properties
+   * */
+  opacity: number,//读取或设置图层的透明度，其值必须在 [0, 1] 区间。
+  blendMode: number//图层的混合模式。
+  isMask: boolean//图层是否是蒙版。
+  effects: any[]//返回一个特效数组，具体数据结构可以查看 Effect。
+  /**
+   * @desc Layout-related properties
+   * */
+  absoluteTransform: Transform//图层节点相对于包含它的页面的位置，以变换矩阵的方式呈现。
+  relativeTransform: Transform//图层节点相对于它的父级节点的位置，作为变换矩阵呈现。
+  x: number,//图层节点的位置，等价于 relativeTransform[0][2]。
+  y: number,//图层节点的位置，等价于 relativeTransform[1][2]。
+  bound: Rect//图层节点的 rect。
+  /** @desc 图层节点的旋转角度.值域为 [-180, 180]。
+   * 其值等价于：Math.atan2(-relativeTransform[1][0], relativeTransform[0][0])
+   * */
+  rotation: number,
+  width: number,
+  height: number
 }
+
+interface Rect {
+  x: number
+  y: number
+  width: number
+  height: number
+}
+
+type Transform = [[number, number, number], [number, number, number]]
 
 export interface EllipseConfig extends ShapeConfig {
   /** @desc 圆弧总长度*/

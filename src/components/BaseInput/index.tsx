668b2314e9026adb283d3a50e64784c7b2af411e
zyronon <zyronon@163.com>
2023-04-04 18:52:55 +0800

    画界面

--- src/components/BaseInput/index.tsx
+++ src/components/BaseInput/index.tsx
@@ -1,6 +1,7 @@
-import {memo, ReactNode} from "react";
+import {memo, ReactNode, useState} from "react";
 import './index.scss'
 import cx from "classnames";
+import {useMemoizedFn} from "ahooks"
 
 interface IProps {
   prefix?: ReactNode,
@@ -14,9 +15,30 @@ export default memo((props: IProps) => {
     prefix, suffix, value = '', onChange = () => {
     }
   } = props
+  const [isMouseDown, setIsMouseDown] = useState<boolean>(false)
+
+  const onMouseDown = useMemoizedFn(() => {
+    setIsMouseDown(true)
+  })
+  const onMouseMove = useMemoizedFn((e) => {
+    if (isMouseDown) {
+
+    }
+  })
+  const onMouseUp = useMemoizedFn(() => {
+    setIsMouseDown(false)
+  })
   return (
     <div className={cx('d-input', !prefix && 'padding')}>
-      {prefix && <div className="prefix">{prefix}</div>}
+      {
+        prefix &&
+        <div className="prefix"
+             onMouseDown={onMouseDown}
+             onMouseMove={onMouseMove}
+             onMouseUp={onMouseUp}>
+          {prefix}
+        </div>
+      }
       <input type="text" value={value} onChange={e => onChange(e)}/>
       {suffix && <div className="suffix">{suffix}</div>}
     </div>

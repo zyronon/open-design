b2fe056687f8b28b64558aab98e60813da561c83
zyronon <zyronon@163.com>
2023-06-08 18:15:13 +0800

    尝试vue

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -1,5 +1,18 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
+import * as VueRouter from "vue-router";
+import design from "./pages/design.vue"
 
-createApp(App).mount('#app')
+const routes = [
+  {path: '/', component: design},
+]
+
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHashHistory(),
+  routes,
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')

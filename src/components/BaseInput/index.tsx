2caee0933469ecd509ce28c762ec5dc4d84d1fa5
zyronon <zyronon@163.com>
2023-06-08 17:55:20 +0800

    改用vue

--- src/components/BaseInput/index.tsx
+++ src/components/BaseInput/index.tsx
@@ -1,5 +1,6 @@
 import {memo, MouseEvent, ReactNode, useRef, useState} from "react";
-import './index.scss'
+// import './index.scss'
+import styles from './index.module.scss'
 import cx from "classnames";
 import {useCreation, useMemoizedFn} from "ahooks"
 
@@ -19,7 +20,6 @@ export default memo((props: IProps) => {
   const start = useRef<number>(0)
 
   const onMouseMove: any = useMemoizedFn((e: MouseEvent) => {
-    console.log('onMouseMove-e', e.clientX)
     console.log('onMouseMove-e', start.current - e.clientX)
   })
 
@@ -41,16 +41,16 @@ export default memo((props: IProps) => {
 
 
   return (
-    <div className={cx('d-input', !prefix && 'padding')}>
+    <div className={cx(styles['d-input'], !prefix && styles['padding'])}>
       {
         prefix &&
-        <div className="prefix"
+        <div className={styles.prefix}
              onMouseDown={onMouseDown}>
           {prefix}
         </div>
       }
       <input type="text" value={value} onChange={e => onChange(e)}/>
-      {suffix && <div className="suffix">{suffix}</div>}
+      {suffix && <div className={styles.suffix}>{suffix}</div>}
     </div>
   )
 })

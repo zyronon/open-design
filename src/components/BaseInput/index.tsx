2ed78f3ddabad759f203cf142c238cd4d3ec7714
zyronon <zyronon@163.com>
2023-06-08 17:20:06 +0800

    输入框

--- src/components/BaseInput/index.tsx
+++ src/components/BaseInput/index.tsx
@@ -1,7 +1,7 @@
-import {memo, ReactNode, useState} from "react";
+import {memo, MouseEvent, ReactNode, useRef, useState} from "react";
 import './index.scss'
 import cx from "classnames";
-import {useMemoizedFn} from "ahooks"
+import {useCreation, useMemoizedFn} from "ahooks"
 
 interface IProps {
   prefix?: ReactNode,
@@ -16,26 +16,36 @@ export default memo((props: IProps) => {
     }
   } = props
   const [isMouseDown, setIsMouseDown] = useState<boolean>(false)
+  const start = useRef<number>(0)
 
-  const onMouseDown = useMemoizedFn(() => {
-    setIsMouseDown(true)
+  const onMouseMove: any = useMemoizedFn((e: MouseEvent) => {
+    console.log('onMouseMove-e', e.clientX)
+    console.log('onMouseMove-e', start.current - e.clientX)
   })
-  const onMouseMove = useMemoizedFn((e) => {
-    if (isMouseDown) {
 
-    }
-  })
-  const onMouseUp = useMemoizedFn(() => {
+  const onMouseUp: any = useMemoizedFn((e: MouseEvent) => {
+    console.log('onMouseUp', e.clientX)
+    window.removeEventListener('mousemove', onMouseMove)
     setIsMouseDown(false)
   })
+
+
+  const onMouseDown = useMemoizedFn((e: MouseEvent) => {
+    console.log('onMouseDown')
+    setIsMouseDown(true)
+    start.current = e.clientX
+    window.addEventListener('mousemove', onMouseMove)
+    window.removeEventListener('mouseup', onMouseUp)
+    window.addEventListener('mouseup', onMouseUp)
+  })
+
+
   return (
     <div className={cx('d-input', !prefix && 'padding')}>
       {
         prefix &&
         <div className="prefix"
-             onMouseDown={onMouseDown}
-             onMouseMove={onMouseMove}
-             onMouseUp={onMouseUp}>
+             onMouseDown={onMouseDown}>
           {prefix}
         </div>
       }
